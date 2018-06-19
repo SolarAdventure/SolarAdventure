2a1f0fc721c4b1c546bdec4e0395492f5211a0a4
ShadowDlux <devin.ruettimann@gmail.com>
2018-06-19 10:21:40 +0200

    Sonne image verbessert

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,46 +10,46 @@ export class AppComponent {
     {
       name: 'Sonne',
       description: 'Ein Stern',
-      img: '../assets/sun.svg',
+      img: '../assets/00_sun.svg',
       isFavourite: false;
     }, {
       name: 'Merkur',
-      description: 'Ein Stern',
+      description: 'Planet: 1, Temperatur: -200 bis 450 Grad, Durchmesser: 4878km',
       img: '../assets/01_mercury.svg',
       isFavourite: false;
     }, {
       name: 'Venus',
-      description: 'Ein Stern',
+      description: 'Planet: 2, Temperatur: 850 Grad, Durchmesser: 4878km',
       img: '../assets/02_venus.svg',
       isFavourite: false;
     }, {
       name: 'Erde',
-      description: 'Ein Stern',
+      description: 'Planet: 3, Temperatur: -200 bis 450 Grad, Durchmesser: 4878km',
       img: '../assets/03_earth.svg',
       isFavourite: false;
     }, {
       name: 'Mars',
-      description: 'Ein Stern',
+      description: 'Planet: 4, Temperatur: -200 bis 450 Grad, Durchmesser: 4878km',
       img: '../assets/04_mars.svg',
       isFavourite: false;
     }, {
       name: 'Jupiter',
-      description: 'Ein Stern',
+      description: 'Planet: 5, Temperatur: -200 bis 450 Grad, Durchmesser: 4878km',
       img: '../assets/05_jupiter.svg',
       isFavourite: false;
     }, {
       name: 'Saturn',
-      description: 'Ein Stern',
+      description: 'Planet: 6, Temperatur: -200 bis 450 Grad, Durchmesser: 4878km',
       img: '../assets/06_saturn.svg',
       isFavourite: false;
     }, {
       name: 'Uranus',
-      description: 'Ein Stern',
+      description: 'Planet: 7, Temperatur: -200 bis 450 Grad, Durchmesser: 4878km',
       img: '../assets/07_uranus.svg',
       isFavourite: false;
     }, {
       name: 'Neptun',
-      description: 'Ein Stern',
+      description: 'Planet: 8, Temperatur: -200 bis 450 Grad, Durchmesser: 4878km',
       img: '../assets/08_neptune.svg',
       isFavourite: false;
     },
