e41c2c48538761a6778723feb63ccedbf20a675e
Luc-Bucher <lucjanbucher@gmail.com>
2018-06-19 10:32:28 +0200

    no ; in object

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,47 +11,47 @@ export class AppComponent {
       name: 'Sonne',
       description: 'Ein Stern',
       img: '../assets/00_sun.svg',
-      isFavourite: false;
+      isFavourite: false
     }, {
       name: 'Merkur',
       description: 'Planet: 1, Temperatur: -200 bis 450 Grad, Durchmesser: 4878km',
       img: '../assets/01_mercury.svg',
-      isFavourite: false;
+      isFavourite: false
     }, {
       name: 'Venus',
       description: 'Planet: 2, Temperatur: 850 Grad, Durchmesser: 4878km',
       img: '../assets/02_venus.svg',
-      isFavourite: false;
+      isFavourite: false
     }, {
       name: 'Erde',
       description: 'Planet: 3, Temperatur: -200 bis 450 Grad, Durchmesser: 4878km',
       img: '../assets/03_earth.svg',
-      isFavourite: false;
+      isFavourite: false
     }, {
       name: 'Mars',
       description: 'Planet: 4, Temperatur: -200 bis 450 Grad, Durchmesser: 4878km',
       img: '../assets/04_mars.svg',
-      isFavourite: false;
+      isFavourite: false
     }, {
       name: 'Jupiter',
       description: 'Planet: 5, Temperatur: -200 bis 450 Grad, Durchmesser: 4878km',
       img: '../assets/05_jupiter.svg',
-      isFavourite: false;
+      isFavourite: false
     }, {
       name: 'Saturn',
       description: 'Planet: 6, Temperatur: -200 bis 450 Grad, Durchmesser: 4878km',
       img: '../assets/06_saturn.svg',
-      isFavourite: false;
+      isFavourite: false
     }, {
       name: 'Uranus',
       description: 'Planet: 7, Temperatur: -200 bis 450 Grad, Durchmesser: 4878km',
       img: '../assets/07_uranus.svg',
-      isFavourite: false;
+      isFavourite: false
     }, {
       name: 'Neptun',
       description: 'Planet: 8, Temperatur: -200 bis 450 Grad, Durchmesser: 4878km',
       img: '../assets/08_neptune.svg',
-      isFavourite: false;
+      isFavourite: false
     },
   ]
 }
