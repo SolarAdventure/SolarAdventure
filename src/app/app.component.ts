f40e280210924bfe5abb65cdf526de151d016c82
ShadowDlux <devin.ruettimann@gmail.com>
2018-06-19 10:45:36 +0200

    planet update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -19,37 +19,37 @@ export class AppComponent {
       isFavourite: false
     }, {
       name: 'Venus',
-      description: 'Planet: 2, Temperatur: 460 Grad, Durchmesser: 4878km',
+      description: 'Planet: 2, Temperatur: 460 Grad, Durchmesser: 12104km',
       img: '../assets/02_venus.svg',
       isFavourite: false
     }, {
       name: 'Erde',
-      description: 'Planet: 3, Temperatur: 15 Grad, Durchmesser: 4878km',
+      description: 'Planet: 3, Temperatur: 15 Grad, Durchmesser: 12760km',
       img: '../assets/03_earth.svg',
       isFavourite: false
     }, {
       name: 'Mars',
-      description: 'Planet: 4, Temperatur: -30 Grad, Durchmesser: 4878km',
+      description: 'Planet: 4, Temperatur: -30 Grad, Durchmesser: 6787km',
       img: '../assets/04_mars.svg',
       isFavourite: false
     }, {
       name: 'Jupiter',
-      description: 'Planet: 5, Temperatur: -110 Grad, Durchmesser: 4878km',
+      description: 'Planet: 5, Temperatur: -110 Grad, Durchmesser: 139822km',
       img: '../assets/05_jupiter.svg',
       isFavourite: false
     }, {
       name: 'Saturn',
-      description: 'Planet: 6, Temperatur: -140 Grad, Durchmesser: 4878km',
+      description: 'Planet: 6, Temperatur: -140 Grad, Durchmesser: 120500km',
       img: '../assets/06_saturn.svg',
       isFavourite: false
     }, {
       name: 'Uranus',
-      description: 'Planet: 7, Temperatur: -195 Grad, Durchmesser: 4878km',
+      description: 'Planet: 7, Temperatur: -195 Grad, Durchmesser: 51120km',
       img: '../assets/07_uranus.svg',
       isFavourite: false
     }, {
       name: 'Neptun',
-      description: 'Planet: 8, Temperatur: -200 Grad, Durchmesser: 4878km',
+      description: 'Planet: 8, Temperatur: -200 Grad, Durchmesser: 49530km',
       img: '../assets/08_neptune.svg',
       isFavourite: false
     },
